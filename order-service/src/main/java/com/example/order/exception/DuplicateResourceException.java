ef69f286ab4d8bdd9c57f8680aca21d1108cba0a
manivannancsmca <manivannancsmca@gmail.com>
2026-09-06 12:23:41 +0530

    global exception added

---
+++ order-service/src/main/java/com/example/order/exception/DuplicateResourceException.java
@@ -0,0 +1,7 @@
+package com.example.order.exception;
+
+public class DuplicateResourceException extends RuntimeException {
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
+}
