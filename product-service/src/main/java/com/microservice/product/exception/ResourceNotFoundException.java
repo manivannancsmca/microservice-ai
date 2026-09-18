14d28a28dce46a4d828dd7b1467975bae6bf6fd2
manivannancsmca <manivannancsmca@gmail.com>
2026-09-18 23:19:09 +0530

    Add constructor to ResourceNotFoundException
    Add constructor to ResourceNotFoundException

--- product-service/src/main/java/com/microservice/product/exception/ResourceNotFoundException.java
+++ product-service/src/main/java/com/microservice/product/exception/ResourceNotFoundException.java
@@ -1,5 +1,7 @@
 package com.microservice.product.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
+    
     public ResourceNotFoundException(String message) { super(message); }
+    
 }
