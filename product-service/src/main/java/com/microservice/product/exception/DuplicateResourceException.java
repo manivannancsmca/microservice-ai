58dc0a3ab84d7f5213788c0bafec8a537eff3884
manivannancsmca <manivannancsmca@gmail.com>
2026-09-18 23:18:46 +0530

    Add constructor to DuplicateResourceException
    Added a constructor to DuplicateResourceException for custom messages.

--- product-service/src/main/java/com/microservice/product/exception/DuplicateResourceException.java
+++ product-service/src/main/java/com/microservice/product/exception/DuplicateResourceException.java
@@ -1,5 +1,7 @@
 package com.microservice.product.exception;
 
 public class DuplicateResourceException extends RuntimeException {
+    
     public DuplicateResourceException(String message) { super(message); }
+    
 }
